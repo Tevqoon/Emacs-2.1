b00642544e3597599a6245f8e4d97930447dd75d
Jure Smolar <jure.smolar@gmail.com>
2026-02-27 13:29:26 +0100

    Hide some agenda tags

--- init.el
+++ init.el
@@ -3498,6 +3498,8 @@ All other subheadings will be ignored."
      (todo . " %i %-12:c")
      (tags . " %i %-12:c")
      (search . " %i %-12:c")))
+  (org-agenda-hide-tags-regexp
+   (regexp-opt '("lecture-notes" "noexport")))
   
   :bind (("C-c a" . open-org-agenda)
 	 :map org-agenda-mode-map
