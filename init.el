5310a98e135999bbea89bc9a1e892a437ee8572c
Jure Smolar <jure.smolar@gmail.com>
2026-04-07 10:11:19 +0200

    Separate out makovec as a separate blog build target

--- init.el
+++ init.el
@@ -4487,6 +4487,12 @@ With C-u C-u: pull static/ from remote."
                     (t "sync-static"))))
       (compile (format "make %s" target))))
 
+  (defun js/makovec-rss ()
+    "Scrape makovec and add to blog"
+    (interactive)
+    (let* ((default-directory (expand-file-name "~/Documents/blog/")))
+      (compile "make makovec")))
+
   (defun js/blog-open-in-browser ()
     "Open the current org-roam file as its published blog URL."
     (interactive)
